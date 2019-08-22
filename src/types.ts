5a1abcd5cb28a0d80fb445d48ca67022ea9a8f62
Sam Pillsworth <sam.pillsworth@shopify.com>
2019-08-22 17:02:37 -0400

    add optional filter flags for added, modified, or deleted files

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,13 @@
 export interface Repository {
-  owner: string;
-  repo: string;
-  issue_number: number;
+    owner: string;
+    repo: string;
+    issue_number: number;
 }
 
 export interface Filter {
-  regExp: string;
-  labels: string[];
+    regExp: string;
+    labels: string[];
+    addedOnly?: boolean;
+    modifiedOnly?: boolean;
+    deletedOnly?: boolean;
 }
